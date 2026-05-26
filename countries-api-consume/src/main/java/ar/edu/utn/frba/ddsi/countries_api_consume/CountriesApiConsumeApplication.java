a1aa88df33d304223a06aaf3d08c106d60ce3922
br4pz <brunapreal@gmail.com>
2026-05-26 14:11:46 -0300

    separate repo

---
+++ countries-api-consume/src/main/java/ar/edu/utn/frba/ddsi/countries_api_consume/CountriesApiConsumeApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.ddsi.countries-api-consume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesApiConsumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesApiConsumeApplication.class, args);
+    }
+
+}
---
+++ countries-api-consume/src/test/java/ar/edu/utn/frba/ddsi/countries_api_consume/CountriesApiConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.ddsi.countries_api_consume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesApiConsumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
